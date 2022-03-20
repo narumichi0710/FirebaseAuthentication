ec4c0a32701a63b620a6acb1bb2c4ec740062fdb
narumichikubo <narumichi7373@gmail.com>
2022-03-20 16:03:21 +0900

    Initial Commit

---
+++ FirebaseAuthentication/FirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthenticationApp.swift
+//  FirebaseAuthentication
+//
+//  Created by Narumichi Kubo on 2022/03/20.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
